769db3ff4e4d8203c78aa581e16d55934a53c130
ZahraMazhari <62299395+ZahraMazhari@users.noreply.github.com>
2020-06-29 22:58:33 +0430

    Add files via upload

---
+++ src/main/java/com/example/FilmYab/DataTransferInterface.java
@@ -0,0 +1,5 @@
+package com.example.FilmYab;
+import android.widget.ImageView;
+public interface DataTransferInterface {
+      public   void movieClick(ImageUrl imageUrl, ImageView imageView);
+}
---
+++ src/main/java/com/example/FilmYab/MovieActivity.java
@@ -0,0 +1,26 @@
+package com.example.FilmYab;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import java.util.Objects;
+public class MovieActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+        String backdrop = Objects.requireNonNull(getIntent().getExtras()).getString("movie-backdrop");
+        String title = Objects.requireNonNull(getIntent().getExtras()).getString("movie-title");
+        String overView = Objects.requireNonNull(getIntent().getExtras()).getString("movie-overview");
+        String releaseDate = Objects.requireNonNull(getIntent().getExtras()).getString("movie-release");
+        ImageView imgBackdrop = findViewById(R.id.backdrop);
+        TextView tvTitle = findViewById(R.id.title);
+        TextView tvOverView = findViewById(R.id.overview);
+        TextView tvReleaseDate = findViewById(R.id.release);
+        tvTitle.setText(title);
+        tvOverView.setText(overView);
+        tvReleaseDate.setText(releaseDate);
+        Glide.with(this).load(backdrop).into(imgBackdrop);
+    }
+}
